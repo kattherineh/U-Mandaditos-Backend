054914d563acc429933c36c07401a3889b915405
Daniel <danyochoa112@gmail.com>
2025-03-18 23:43:21 -0600

    feat: creacion de entidad User y su respectivo repositorio y relaciones en db

---
+++ Backend/Aplication/Interfaces/Users/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Aplication.Interfaces.Users;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllAsync();
+    Task<User?> GetByIdAsync(int id);
+    Task AddAsync(User user);
+    Task<bool> UpdateAsync(User user);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ Backend/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Aplication.Interfaces.Users;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private BackendDbContext _dbContext;
+
+    public UserRepository(BackendDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _dbContext.Users.ToListAsync();
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _dbContext.Users.FindAsync(id);
+    }
+
+    public async Task AddAsync(User user)
+    { 
+        _dbContext.Users.Add(user);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> UpdateAsync(User user)
+    {
+        _dbContext.Users.Update(user);
+        return await _dbContext.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var user = await _dbContext.Users.FindAsync(id);
+        if (user is null) return false;
+        _dbContext.Users.Remove(user);
+        return await _dbContext.SaveChangesAsync() > 0;
+    }
+}
