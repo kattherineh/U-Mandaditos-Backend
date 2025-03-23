ca907e609c7481f905d7968bf7972a75043cf2cd
Daniel <danyochoa112@gmail.com>
2025-03-22 20:10:07 -0600

    fix: se corrige el repositorio de Post y se incluyen las entidades con las que se relacina

--- Backend/Domain/AggregateRoot/Post.cs
+++ Backend/Domain/AggregateRoot/Post.cs
@@ -8,14 +8,14 @@ public class Post: AggregateRoot
 
     public double SugestedValue { get; set; }
     
-    public User PosterUser { get; set; }
     public int? IdPosterUser { get; set; }
+    public User PosterUser { get; set; }
     
-    public Location PickUpLocation { get; set; }
     public int? IdPickUpLocation { get; set; }
+    public Location PickUpLocation { get; set; }
     
-    public  Location DeliveryLocation { get; set; }
     public int?  IdDeliveryLocation { get; set; }
+    public  Location DeliveryLocation { get; set; }
     
     public  DateTime CreatedAt { get; set; }
     
