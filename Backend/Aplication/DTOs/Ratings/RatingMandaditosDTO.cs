5db7ef9d66ba856a0fd99df703a056a89756a261
Daniel <danyochoa112@gmail.com>
2025-04-03 20:43:24 -0600

    fix: arreglos en el tipo de dato de las fechas de los DTOS

---
+++ Backend/Aplication/DTOs/Ratings/RatingMandaditosDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Aplication.DTOs;
+
+public class RatingMandaditosDTO
+{
+    public string Review { get; set; } = string.Empty;
+    public string DatePosted { get; set; } = string.Empty;
+    public int Rating { get; set; }
+    public bool IsRunner { get; set; } 
+
+}
