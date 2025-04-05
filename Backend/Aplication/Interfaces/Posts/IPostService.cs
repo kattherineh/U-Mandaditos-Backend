58d48903ec4df0649abb1e38c4ffbd45f97ff5f5
Daniel <danyochoa112@gmail.com>
2025-04-05 14:37:29 -0600

    fix: se agregaron propiedades necesarias en el servicio de obtener post por locacion

--- Backend/Aplication/Interfaces/Posts/IPostService.cs
+++ Backend/Aplication/Interfaces/Posts/IPostService.cs
@@ -7,7 +7,7 @@ public interface IPostService
 {
     
     Task<ResponseDTO<PostResponseDTO>> CreateAsync(PostRequestDTO dto);
-    Task<IEnumerable<PostResponseDTO>> GetAllNearAsync(int currentLocationId);
+    Task<ResponseDTO<IEnumerable<PostResponseDTO>>> GetAllNearAsync(int currentLocationId);
     Task<IEnumerable<PostResponseDTO>> GetPostsNearLocationAsync(int idLocation);
     Task<IEnumerable<PostResponseDTO>> GetPostsByPosterUserIdAsync(int idPosterUser);
     Task<int> GetPostsCountAsync(int idPosterUser);
