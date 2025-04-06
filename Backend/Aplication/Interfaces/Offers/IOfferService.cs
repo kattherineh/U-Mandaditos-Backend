0adfeb8c169103c01d69e1d9888e1a22c3b86468
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2025-04-06 00:32:15 -0600

    fix: se corrijieron servicios para poder crear ofertas

--- Backend/Aplication/Interfaces/Offers/IOfferService.cs
+++ Backend/Aplication/Interfaces/Offers/IOfferService.cs
@@ -1,11 +1,12 @@
 using Aplication.DTOs;
+using Aplication.DTOs.General;
 
 
 namespace Aplication.Interfaces {
     public interface IOfferService {
         Task<OfferResponseDTO?> GetOfferByIdAsync(int id);
         Task<IEnumerable<OfferResponseDTO>> GetOffersByPostIdAsync(int idPost);
-        Task<OfferResponseDTO> CreateOfferAsync(OfferRequestDTO dto);
+        Task<ResponseDTO<OfferResponseDTO>> CreateOfferAsync(OfferRequestDTO offerRequest);
         Task<OfferResponseDTO?> UpdateOfferStateAsync(int id, bool isAccepted);
         Task<int> QuantityOffersAcceptedByUserAsync(int idUser);
         Task<bool> DeleteOfferAsync(int id);
